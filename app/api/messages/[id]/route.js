92dbbd2173f8ac86fd01059cb77389f32890138a
Tushar <tusharpersai@gmail.com>
2024-04-11 23:30:46 +0530

    messageDisplay

---
+++ app/api/messages/[id]/route.js
@@ -0,0 +1,75 @@
+import connectDB from "@/config/database";
+import Message from "@/model/Message";
+import { getSessionUser } from "@/utils/getSessionUser";
+
+export const dynamic = 'force-dynamic';
+
+//PUT /api/messages/:id
+export const PUT = async(request,{params})=>{
+
+    try {
+        await connectDB();
+        const {id} = params;
+        const sessionUser = await getSessionUser();
+
+        if(!sessionUser || !sessionUser.user){
+
+            return new Response('User Id is required',{status:401});
+
+        }
+        const {userId} = sessionUser;
+        const message = await Message.findById(id);
+        if(!message){
+
+            return new Response(JSON.stringify({message:'Message not found'}),{status:404});
+        }
+
+        //Verify Ownership
+        if(message.recipient.toString() !== userId){
+                
+                return new Response('Unauthorized',{status:401});
+
+        }
+        //Update read or unread depending on current status
+        message.read = !message.read;
+        await message.save();
+        return new Response(JSON.stringify(message),{status:200});
+    } catch (error) {
+        console.log(error);
+        return new Response('Something went wrong',{status:500});
+    }
+}
+export const DELETE = async(request,{params})=>{
+
+    try {
+        await connectDB();
+        const {id} = params;
+        const sessionUser = await getSessionUser();
+
+        if(!sessionUser || !sessionUser.user){
+
+            return new Response('User Id is required',{status:401});
+
+        }
+        const {userId} = sessionUser;
+        const message = await Message.findById(id);
+        if(!message){
+
+            return new Response(JSON.stringify({message:'Message not found'}),{status:404});
+        }
+
+        //Verify Ownership
+        if(message.recipient.toString() !== userId){
+                
+                return new Response('Unauthorized',{status:401});
+
+        }
+        //Update read or unread depending on current status
+       
+        await message.deleteOne();
+        return new Response('Message Deleted',{status:200});
+    } catch (error) {
+        console.log(error);
+        return new Response('Something went wrong',{status:500});
+    }
+}
--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropertyCard from '@/components/PropertyCard';
 import { fetchProperties } from '@/utils/requests';
+import PropertySearchForm from '@/components/PropertySearchForm';
 
 
 
@@ -9,7 +10,17 @@ const PropertiesPage = async () => {
   
   //Sort properties by date
   properties.sort((a,b)=>new Date(b.createdAt) - new Date(a.createdAt));
-  return (
+  
+
+  return (<>
+  
+    <section className="bg-blue-700 py-4">
+      <div className="max-w-7xl mx-auto px-4 flex-col items-start sm:px-6 lg:px-8">
+          <PropertySearchForm />
+      </div>
+    </section>
+    
+  
     <section className="px-4 py-6">
     <div className="container-xl lg:container m-auto px-4 py-6">
       { properties.length === 0 ? (
@@ -23,8 +34,9 @@ const PropertiesPage = async () => {
       )}
     </div>
         </section>
-     
-  )
+  
+    
+    </>)
 }
 
 export default PropertiesPage
